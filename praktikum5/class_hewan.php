434e38f46c443560e8cb1f1c318de2855d84cc4b
gihal <146146734+agihalputra@users.noreply.github.com>
2024-03-25 09:18:06 +0700

    Add files via upload

---
+++ praktikum5/class_hewan.php
@@ -0,0 +1,45 @@
+<?php
+
+    class HewanMamalia
+    {
+        public $warna;
+        public $namaHewan;
+        public $habitat;
+
+        function makan()
+        {
+            return "Mamalia makan";
+        }
+
+        function minum()
+        {
+            return "Mamalia minum";
+        } 
+
+        function menyusui()
+        {
+            return "Mamalia menyusui";
+        }
+
+        function melahirkan()
+        {
+            return "Mamalia menyusui";
+        }
+
+
+    }
+
+$mamalia = new HewanMamalia();
+echo $mamalia->namaHewan = "Lumba-Lumba";
+echo "<br>";
+echo $mamalia->warna = "Abu-abu";
+echo "<br>";
+echo $mamalia->habitat = "Laut";
+echo "<br>";
+echo $mamalia->makan();
+echo "<br>";
+echo $mamalia->minum();
+echo "<br>";
+echo $mamalia->menyusui();
+echo "<br>";
+echo $mamalia->melahirkan();
---
+++ praktikum5/class_mahasiswa.php
@@ -0,0 +1,39 @@
+<?php
+
+class Mahasiswa
+{
+    public $namaMahasiswa, $nim, $domisili, $prodi, $ipk;
+    
+    function __construct($namaMahasiswa, $nim, $domisili, $prodi, $ipk)
+    {
+        $this->namaMahasiswa = $namaMahasiswa;
+        $this->nim = $nim;
+        $this->domisili = $domisili;
+        $this->prodi = $prodi;
+        $this->ipk = $ipk;
+
+    }
+
+    function setPredikatIPK($ipk)
+    {
+        if ($ipk > 3.5) {
+            echo "cumlaude";
+        } else {
+            echo "You did a great job!";
+        }
+
+    }
+}
+$mahasiswa = new Mahasiswa("Agihal Putra", "0110223083","Bojonggede","TI", "3.8");
+echo "Nama Lengkap :". $mahasiswa->namaMahasiswa;
+echo "<br>";
+echo "NIM :". $mahasiswa->nim;
+echo "<br>";
+echo "Domisili :". $mahasiswa->domisili;
+echo "<br>";
+echo "Prodi :". $mahasiswa-> prodi;
+echo "<br>";
+echo "IPK :". $mahasiswa->ipk;
+echo "<br>";
+
+$mahasiswa ->setPredikatIPK($mahasiswa->ipk);
